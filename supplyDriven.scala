8a834130df17f936957dfc04e1d56ea5abf0ec8e
Tate <dracoranger@hotmail.com>
2018-08-23 19:55:11 -0400

    More poddling along

--- supplyDriven.scala
+++ supplyDriven.scala
@@ -1,5 +1,7 @@
 import scala.math
+import scala.collection.mutable.Stack
 import java.io._
+
 
 object main{
   def main(){
@@ -31,21 +33,43 @@ class City(var locl:Int){
                               new Industry("stone" Array(1.0,1.0,0.0), Array(0.0,0.0,3.0), 600, 600.0, 1.0))
   def next_fiscal_quarter():Int={
     val storage = Array(resources_array(0).stored,resources_array(1).stored,resources_array(2).stored)
-    val demand = Array.fill(3,0)
-    var excess_demand = False
+    val demand = Array.fill[Double](3)(0)
+    val total_workers = 0
+    var excess_demand = Stack[Boolean]()
+    var excess_demand_vals = Stack[Int]()
+    var how_much = Stack[Double]()
     for (i <- resources_array){
       demand(0) = demand(0) + i.inputs(0)*i.workers
       demand(1) = demand(1) + i.inputs(1)*i.workers
       demand(2) = demand(2) + i.inputs(2)*i.workers
+      total_workers = total_workers + i.workers
     }
-    for(var i <- 0 until 3){
+
+    for(i <- 0 to 3){
       if(demand(i)>storage(i)){
-        excess_demand = True
+        excess_demand.push(true)
+        excess_demand_vals.push(i)
+      }
+      else{
+        excess_demand.push(false)
+      }
+      how_much.push(demand(i)-storage(i))
+    }
+
+    while(!excess_demand.isEmpty){
+      if(excess_demand.pop){
+        var resource_demanded = excess_demand_vals.pop
+        for(i <- resources_array){
+          if(i.input(resource_demanded)>0){
+
+          }
+        }
+      }
+      else{
+
       }
     }
-    if(excess_demand){
-
-    }
+    return 1
   }
 
   def tickFinish(){
